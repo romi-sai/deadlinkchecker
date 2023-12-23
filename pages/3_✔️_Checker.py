e4b7619dd2d78537f4bbf20cc166e0c6d081fed8
romi-sai <54590905+romi-sai@users.noreply.github.com>
2023-12-23 14:43:00 -0500

    Update 3_✔️_Checker.py

--- pages/3_✔️_Checker.py
+++ pages/3_✔️_Checker.py
@@ -1,8 +1,6 @@
+import streamlit as st
 import requests
-try:
-    from bs4 import BeautifulSoup
-except :
-    from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import asyncio
 
@@ -29,7 +27,7 @@ async def check_single_link(url, depth, max_depth, visited_links, broken_links):
             results = await asyncio.gather(*tasks)
 
     except Exception as e:
-        #st.warning(f"Error checking link {url}: {e}")
+        # st.warning(f"Error checking link {url}: {e}")
         pass
 
 async def main(url, depth):
@@ -41,10 +39,25 @@ async def main(url, depth):
 
     st.text(f"Total links checked: {len(visited_links)}")
     st.text(f"Passed links: {len(visited_links) - len(broken_links)}")
-	@@ -61,6 +57,6 @@ async def main(url, depth):
+    st.error(f"Dead links: {len(broken_links)}")
+
+    if broken_links:
+        st.error("Link checking completed. Broken links:")
+        for link in broken_links:
+            st.warning(link)
+    else:
+        st.success("Link checking completed! No broken links found!")
+
+# Streamlit UI
+st.set_page_config(page_title="Deadlink Checker", page_icon="🔗", layout="wide")
+st.title('Deadlink Checker!')
+
+url = st.text_input("Enter URL to check for dead links:")
+given = st.selectbox("Please select the depth you'd like to check", ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+
 if st.button("Check"):
     if url:
         with st.spinner("Checking links..."):
-            asyncio.run(main(url, int(given)))
+            await main(url, int(given))
     else:
         st.warning("Please enter a valid URL.")
